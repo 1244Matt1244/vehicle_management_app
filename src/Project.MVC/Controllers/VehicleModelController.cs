6a0f760950d50bbe0cb7a203d18b3afd1627e6ea
Matej Martinović <matt123.3a@gmail.com>
2025-02-20 03:04:49 +0100

    Clean up: remove unnecessary files and update project structure

--- src/Project.MVC/Controllers/VehicleModelController.cs
+++ src/Project.MVC/Controllers/VehicleModelController.cs
@@ -1,23 +1,39 @@
+// Project.MVC/Controllers/VehicleModelController.cs
 using Microsoft.AspNetCore.Mvc;
 using Project.Service.Interfaces;
-using Project.Service.Data.DTOs;
+using Project.MVC.ViewModels;
+using AutoMapper;
 
 namespace Project.MVC.Controllers
 {
     public class VehicleModelController : Controller
     {
         private readonly IVehicleService _vehicleService;
+        private readonly IMapper _mapper;
 
-        public VehicleModelController(IVehicleService vehicleService)
+        public VehicleModelController(IVehicleService vehicleService, IMapper mapper)
         {
             _vehicleService = vehicleService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
-            var makes = await _vehicleService.GetAllMakesAsync();
-            ViewBag.Makes = makes; // If you want to stick with ViewBag
-            return View(makes); // Pass makes directly to the view
+            var models = await _vehicleService.GetModelsAsync();
+            var viewModels = _mapper.Map<IEnumerable<VehicleModelVM>>(models);
+            return View(viewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(VehicleModelVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dto = _mapper.Map<VehicleModelDTO>(model);
+                await _vehicleService.CreateModelAsync(dto);
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
