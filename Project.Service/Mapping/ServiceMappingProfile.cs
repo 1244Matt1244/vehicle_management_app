12dc5e06cae22198c9cf62e160aa908a4ce78d67
Matej Martinović <matt123.3a@gmail.com>
2025-02-21 16:17:15 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.Service/Mapping/ServiceMappingProfile.cs
+++ Project.Service/Mapping/ServiceMappingProfile.cs
@@ -1,19 +1,19 @@
+// Project.Service/Mapping/ServiceMappingProfile.cs
 using AutoMapper;
-using Project.Service.Shared.Models;
-using Project.Service.Shared.DTOs;
+using Project.Service.DTOs;
+using Project.Service.Models;
 
-namespace Project.Service.Shared.Mapping
+namespace Project.Service.Mapping
 {
     public class ServiceMappingProfile : Profile
     {
         public ServiceMappingProfile()
         {
-            CreateMap<VehicleMake, VehicleMakeDTO>()
-                .ReverseMap();
-
+            CreateMap<VehicleMake, VehicleMakeDTO>().ReverseMap();
             CreateMap<VehicleModel, VehicleModelDTO>()
                 .ForMember(dest => dest.MakeName, opt => opt.MapFrom(src => src.Make.Name))
-                .ReverseMap();
+                .ReverseMap()
+                .ForMember(dest => dest.Make, opt => opt.Ignore()); // Avoid circular reference
         }
     }
 }
--- Project.Service/Services/VehicleService.cs
+++ Project.Service/Services/VehicleService.cs
@@ -1,11 +1,10 @@
+\// Project.Service/Services/VehicleService.cs
 using AutoMapper;
-using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 using Project.Service.Data.Context;
-using Project.Service.Shared.DTOs;
-using Project.Service.Shared.Helpers;
+using Project.Service.DTOs;
 using Project.Service.Interfaces;
-using Project.Service.Shared.Models;
+using Project.Service.Models;
 
 namespace Project.Service.Services
 {
@@ -20,74 +19,30 @@ namespace Project.Service.Services
             _mapper = mapper;
         }
 
-        public async Task<PaginatedList<VehicleMakeDTO>> GetMakesAsync(int pageIndex, int pageSize, 
-            string sortBy, string sortOrder, string search)
+        // Vehicle Make Methods
+        public async Task<IEnumerable<VehicleMakeDTO>> GetAllMakesAsync()
         {
-            var query = _context.VehicleMakes.AsNoTracking();
-
-            if (!string.IsNullOrEmpty(search))
-                query = query.Where(m => m.Name.Contains(search) || m.Abrv.Contains(search));
-
-            query = sortOrder == "desc" 
-                ? query.OrderByDescending(m => EF.Property<object>(m, sortBy))
-                : query.OrderBy(m => EF.Property<object>(m, sortBy));
-
-            var totalCount = await query.CountAsync();
-            var items = await query
-                .Skip((pageIndex - 1) * pageSize)
-                .Take(pageSize)
-                .ProjectTo<VehicleMakeDTO>(_mapper.ConfigurationProvider)
-                .ToListAsync();
-
-            return new PaginatedList<VehicleMakeDTO>(items, totalCount, pageIndex, pageSize);
-        }
-
-        public async Task<PaginatedList<VehicleModelDTO>> GetModelsAsync(int pageIndex, int pageSize, 
-            string sortBy, string sortOrder, string search, int? makeId)
-        {
-            var query = _context.VehicleModels
-                .Include(m => m.Make)
-                .AsNoTracking();
-
-            if (makeId.HasValue)
-                query = query.Where(m => m.MakeId == makeId.Value);
-
-            if (!string.IsNullOrEmpty(search))
-                query = query.Where(m => m.Name.Contains(search) || m.Make.Name.Contains(search));
-
-            query = sortOrder == "desc" 
-                ? query.OrderByDescending(m => EF.Property<object>(m, sortBy))
-                : query.OrderBy(m => EF.Property<object>(m, sortBy));
-
-            var totalCount = await query.CountAsync();
-            var items = await query
-                .Skip((pageIndex - 1) * pageSize)
-                .Take(pageSize)
-                .ProjectTo<VehicleModelDTO>(_mapper.ConfigurationProvider)
-                .ToListAsync();
-
-            return new PaginatedList<VehicleModelDTO>(items, totalCount, pageIndex, pageSize);
+            var makes = await _context.VehicleMakes.ToListAsync();
+            return _mapper.Map<IEnumerable<VehicleMakeDTO>>(makes);
         }
 
         public async Task<VehicleMakeDTO> GetMakeByIdAsync(int id)
         {
             var make = await _context.VehicleMakes.FindAsync(id);
-            return make == null ? null : _mapper.Map<VehicleMakeDTO>(make);
+            return _mapper.Map<VehicleMakeDTO>(make);
         }
 
         public async Task CreateMakeAsync(VehicleMakeDTO makeDto)
         {
-            var entity = _mapper.Map<VehicleMake>(makeDto);
-            _context.VehicleMakes.Add(entity);
+            var make = _mapper.Map<VehicleMake>(makeDto);
+            _context.VehicleMakes.Add(make);
             await _context.SaveChangesAsync();
         }
 
         public async Task UpdateMakeAsync(VehicleMakeDTO makeDto)
         {
-            var entity = await _context.VehicleMakes.FindAsync(makeDto.Id);
-            if (entity == null) return;
-
-            _mapper.Map(makeDto, entity);
+            var make = _mapper.Map<VehicleMake>(makeDto);
+            _context.VehicleMakes.Update(make);
             await _context.SaveChangesAsync();
         }
 
@@ -101,27 +56,30 @@ namespace Project.Service.Services
             }
         }
 
+        // Vehicle Model Methods
+        public async Task<IEnumerable<VehicleModelDTO>> GetAllModelsAsync()
+        {
+            var models = await _context.VehicleModels.Include(m => m.Make).ToListAsync();
+            return _mapper.Map<IEnumerable<VehicleModelDTO>>(models);
+        }
+
         public async Task<VehicleModelDTO> GetModelByIdAsync(int id)
         {
-            var model = await _context.VehicleModels
-                .Include(m => m.Make)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            return model == null ? null : _mapper.Map<VehicleModelDTO>(model);
+            var model = await _context.VehicleModels.Include(m => m.Make).FirstOrDefaultAsync(m => m.Id == id);
+            return _mapper.Map<VehicleModelDTO>(model);
         }
 
         public async Task CreateModelAsync(VehicleModelDTO modelDto)
         {
-            var entity = _mapper.Map<VehicleModel>(modelDto);
-            _context.VehicleModels.Add(entity);
+            var model = _mapper.Map<VehicleModel>(modelDto);
+            _context.VehicleModels.Add(model);
             await _context.SaveChangesAsync();
         }
 
         public async Task UpdateModelAsync(VehicleModelDTO modelDto)
         {
-            var entity = await _context.VehicleModels.FindAsync(modelDto.Id);
-            if (entity == null) return;
-
-            _mapper.Map(modelDto, entity);
+            var model = _mapper.Map<VehicleModel>(modelDto);
+            _context.VehicleModels.Update(model);
             await _context.SaveChangesAsync();
         }
 
