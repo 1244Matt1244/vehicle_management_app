c34a92f8e4fc91509f1669be7136a9c5f15c54a8
Matej Martinović <matt123.3a@gmail.com>
2025-02-24 23:44:08 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.Service/Models/VehicleMake.cs
+++ Project.Service/Models/VehicleMake.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 namespace Project.Service.Models
 {
     public class VehicleMake
-{
+    {
     public required int Id { get; set; }
     public required string Name { get; set; }
     public required string Abrv { get; set; }
-
-    // Navigation property for VehicleModels
-    public ICollection<VehicleModel> VehicleModels { get; set; } = new List<VehicleModel>();
-}
+    }
 }
