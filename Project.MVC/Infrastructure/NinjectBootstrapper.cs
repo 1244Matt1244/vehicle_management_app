adbbfa1feb03a6965a917ad7287695464b069612
Matej Martinović <matt123.3a@gmail.com>
2025-02-24 20:20:18 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.MVC/Infrastructure/NinjectBootstrapper.cs
+++ Project.MVC/Infrastructure/NinjectBootstrapper.cs
@@ -1,11 +1,4 @@
-using System.Collections.Generic;
 using Ninject.Modules;
-using Microsoft.EntityFrameworkCore;
-using Project.Service.Data.Context;
-using Project.Service.Interfaces;
-using Project.Service.Data.Helpers;
-using Project.Service.Repositories;
-using Project.Service.Services;
 using AutoMapper;
 using Project.Service.Mappings;
 using Project.MVC.Mappings;
@@ -16,28 +9,12 @@ namespace Project.MVC.Infrastructure
     {
         public override void Load()
         {
-            // Database context (scoped per request)
-            Bind<ApplicationDbContext>()
-                .ToSelf()
-                .InTransientScope() // Use Transient for EF Core
-                .WithConstructorArgument("options",
-                    new DbContextOptionsBuilder<ApplicationDbContext>()
-                        .UseSqlite("Data Source=vehicles.db")
-                        .Options);
-
-            // Repository and service bindings
-            Bind<IVehicleRepository>().To<VehicleRepository>();
-            Bind<IVehicleService>().To<VehicleService>();
-
             // AutoMapper configuration
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.AddProfile<ServiceMappingProfile>();
-                cfg.AddProfile<MvcMappingProfile>();
-                cfg.CreateMap(typeof(PaginatedList<>), typeof(PaginatedList<>))
-                    .ConvertUsing(typeof(PaginatedListConverter<,>));
+                cfg.AddProfile<ServiceMappingProfile>(); // Service layer mappings
+                cfg.AddProfile<MvcMappingProfile>();     // MVC view model mappings
             });
-            
             Bind<IMapper>().ToConstant(config.CreateMapper());
         }
     }
