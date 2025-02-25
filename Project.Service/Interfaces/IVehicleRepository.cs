73ec5c744b47b9a9576a0b314a55bb583a0c5199
Matej Martinović <matt123.3a@gmail.com>
2025-02-25 02:08:21 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.Service/Interfaces/IVehicleRepository.cs
+++ Project.Service/Interfaces/IVehicleRepository.cs
@@ -7,22 +7,20 @@ namespace Project.Service.Interfaces
 {
     public interface IVehicleRepository
     {
-        // VehicleMake
-        Task<PaginatedList<VehicleMake>> GetMakesPaginatedAsync(int page, int pageSize, string sortBy, string sortOrder, string searchString);
+        // Vehicle Make Methods
+        Task<(List<VehicleMake> Makes, int TotalCount)> GetMakesPaginatedAsync(int page, int pageSize, string sortBy, string sortOrder, string searchString);
         Task<VehicleMake?> GetMakeByIdAsync(int id);
         Task AddMakeAsync(VehicleMake make);
         Task UpdateMakeAsync(VehicleMake make);
         Task DeleteMakeAsync(VehicleMake make);
+        Task<List<VehicleMake>> GetAllMakesAsync();
 
-        // VehicleModel
-        Task<PaginatedList<VehicleModel>> GetModelsPaginatedAsync(int page, int pageSize, string sortBy, string sortOrder, string searchString, int? makeId);
+        // Vehicle Model Methods
+        Task<(List<VehicleModel> Models, int TotalCount)> GetModelsPaginatedAsync(int page, int pageSize, string sortBy, string sortOrder, string searchString, int? makeId);
         Task<VehicleModel?> GetModelByIdAsync(int id);
         Task AddModelAsync(VehicleModel model);
         Task UpdateModelAsync(VehicleModel model);
         Task DeleteModelAsync(VehicleModel model);
-
-        // Common
-        Task<List<VehicleMake>> GetAllMakesAsync();
         Task<List<VehicleModel>> GetAllModelsAsync();
     }
 }
