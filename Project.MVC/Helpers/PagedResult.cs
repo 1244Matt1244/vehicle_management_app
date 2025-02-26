76a4059d19393072d96920c125f5616e9879deb6
Matej Martinović <matt123.3a@gmail.com>
2025-02-26 20:18:05 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.MVC/Helpers/PagedResult.cs
+++ Project.MVC/Helpers/PagedResult.cs
@@ -1,10 +1,10 @@
-namespace Project.MVC.Helpers
-{
-    public class PagedResult<T>
-    {
-        public List<T> Items { get; set; }
-        public int TotalCount { get; set; }
-        public int PageNumber { get; set; }  // Match PaginatedList.PageIndex
-        public int PageSize { get; set; }
-    }
-}
+namespace Project.MVC.Helpers  
+{  
+    public class PagedResult<T>  
+    {  
+        public List<T> Items { get; set; }  
+        public int TotalCount { get; set; }  
+        public int PageIndex { get; set; }  // Renamed from PageNumber  
+        public int PageSize { get; set; }  
+    }  
+}  
--- Project.MVC/Mappings/MvcMappingProfile.cs
+++ Project.MVC/Mappings/MvcMappingProfile.cs
@@ -1,47 +1,47 @@
-using AutoMapper;
-using Project.MVC.ViewModels;
-using Project.Service.Data.DTOs;
-using Project.Service.Data.Helpers;
+using AutoMapper;  
+using Project.MVC.Helpers;  
+using Project.Service.Data.DTOs;  
+using Project.Service.Data.Helpers;  
+using System.Collections.Generic;  
 
-namespace Project.MVC.Mappings
-{
-    public class MvcMappingProfile : Profile
-    {
-        public MvcMappingProfile()
-        {
-            // VehicleMake mappings
-            CreateMap<VehicleMakeDTO, VehicleMakeVM>().ReverseMap();
+namespace Project.MVC.Mappings  
+{  
+    public class MvcMappingProfile : Profile  
+    {  
+        public MvcMappingProfile()  
+        {  
+            // VehicleMake mappings  
+            CreateMap<VehicleMakeDTO, VehicleMakeVM>().ReverseMap();  
+
+            // VehicleModel mappings  
+            CreateMap<VehicleModelDTO, VehicleModelVM>()  
+                .ForMember(dest => dest.MakeName, opt => opt.MapFrom(src => src.Make.Name))  
+                .ReverseMap();  
+
+            // Pagination mapping  
+            CreateMap<PaginatedList<VehicleMakeDTO>, PagedResult<VehicleMakeVM>>()  
+                .ConvertUsing<PaginatedListConverter<VehicleMakeDTO, VehicleMakeVM>>();  
             
-            // VehicleModel mappings
-            CreateMap<VehicleModelDTO, VehicleModelVM>()
-                .ForMember(dest => dest.MakeName, opt => opt.MapFrom(src => src.Make.Name)) // Fix mapping
-                .ReverseMap();
+            CreateMap<PaginatedList<VehicleModelDTO>, PagedResult<VehicleModelVM>>()  
+                .ConvertUsing<PaginatedListConverter<VehicleModelDTO, VehicleModelVM>>();  
+        }  
+    }  
 
-            // Pagination type conversion (Resolve PaginatedList -> PagedResult)
-            CreateMap<PaginatedList<VehicleMakeDTO>, PagedResult<VehicleMakeVM>>()
-                .ConvertUsing<PaginatedListConverter<VehicleMakeDTO, VehicleMakeVM>>();
-            
-            CreateMap<PaginatedList<VehicleModelDTO>, PagedResult<VehicleModelVM>>()
-                .ConvertUsing<PaginatedListConverter<VehicleModelDTO, VehicleModelVM>>();
-        }
-    }
-
-    // Define the converter within the Mappings namespace
-    public class PaginatedListConverter<TSource, TDestination> 
-        : ITypeConverter<PaginatedList<TSource>, PagedResult<TDestination>>
-    {
-        public PagedResult<TDestination> Convert(
-            PaginatedList<TSource> source, 
-            PagedResult<TDestination> destination, 
-            ResolutionContext context)
-        {
-            return new PagedResult<TDestination>
-            {
-                Items = context.Mapper.Map<List<TDestination>>(source.Items),
-                TotalCount = source.TotalCount,
-                PageNumber = source.PageIndex, // Align with your PagedResult properties
-                PageSize = source.PageSize
-            };
-        }
-    }
-}
+    public class PaginatedListConverter<TSource, TDestination>  
+        : ITypeConverter<PaginatedList<TSource>, PagedResult<TDestination>>  
+    {  
+        public PagedResult<TDestination> Convert(  
+            PaginatedList<TSource> source,  
+            PagedResult<TDestination> destination,  
+            ResolutionContext context)  
+        {  
+            return new PagedResult<TDestination>  
+            {  
+                Items = context.Mapper.Map<List<TDestination>>(source.Items),  
+                TotalCount = source.TotalCount,  
+                PageIndex = source.PageIndex,  // Align property names  
+                PageSize = source.PageSize  
+            };  
+        }  
+    }  
+}  
