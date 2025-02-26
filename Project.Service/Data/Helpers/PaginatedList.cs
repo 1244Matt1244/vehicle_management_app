fbe18661445fe5f7061171462a44d89384d3a3e1
Matej Martinović <matt123.3a@gmail.com>
2025-02-26 20:18:05 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.Service/Data/Helpers/PaginatedList.cs
+++ Project.Service/Data/Helpers/PaginatedList.cs
@@ -1,3 +1,4 @@
+// Project.Service/Data/Helpers/PaginatedList.cs
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,11 +9,10 @@ namespace Project.Service.Data.Helpers
     public class PaginatedList<T>
     {
         public List<T> Items { get; }
+        public int TotalCount { get; }
         public int PageIndex { get; }
         public int PageSize { get; }
-        public int TotalCount { get; }
-        public int TotalPages => (TotalCount + PageSize - 1) / PageSize;
-
+        
         public PaginatedList(List<T> items, int totalCount, int pageIndex, int pageSize)
         {
             Items = items;
@@ -21,14 +21,11 @@ namespace Project.Service.Data.Helpers
             PageSize = pageSize;
         }
 
-        public static async Task<PaginatedList<T>> CreateAsync(
-            IQueryable<T> source, int pageIndex, int pageSize)
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
-            var totalCount = await source.CountAsync();
-            var items = await source.Skip((pageIndex - 1) * pageSize)
-                                    .Take(pageSize)
-                                    .ToListAsync();
-            return new PaginatedList<T>(items, totalCount, pageIndex, pageSize);
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
 }
--- Project.Service/Mappings/ServiceMappingProfile.cs
+++ Project.Service/Mappings/ServiceMappingProfile.cs
@@ -1,3 +1,4 @@
+// Project.Service/Mappings/ServiceMappingProfile.cs
 using AutoMapper;
 using Project.Service.Data.DTOs;
 using Project.Service.Models;
@@ -8,17 +9,12 @@ namespace Project.Service.Mappings
     {
         public ServiceMappingProfile()
         {
-            // VehicleMake mappings
-            CreateMap<VehicleMake, VehicleMakeDTO>()
-                .ReverseMap();
-
-            // VehicleModel mappings with correct navigation property
+            CreateMap<VehicleMake, VehicleMakeDTO>().ReverseMap();
+            
             CreateMap<VehicleModel, VehicleModelDTO>()
-                .ForMember(dest => dest.MakeName, 
-                          opt => opt.MapFrom(src => src.VehicleMake.Name))  // Fixed to use VehicleMake
+                .ForMember(dest => dest.MakeName, opt => opt.MapFrom(src => src.VehicleMake.Name))
                 .ReverseMap()
-                .ForPath(src => src.VehicleMake.Name, 
-                        opt => opt.Ignore());  // Prevent overwrite of navigation property
+                .ForPath(src => src.VehicleMake.Name, opt => opt.Ignore());
         }
     }
 }
