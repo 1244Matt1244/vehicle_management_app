549cc35c0a404eb59ca4409db05cceb607a339f9
Matej Martinović <matt123.3a@gmail.com>
2025-02-27 17:22:25 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.MVC/Program.cs
+++ Project.MVC/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Project.Service.Interfaces;
 using Project.Service.Services;
 using Project.Service.Data.Context;
@@ -12,8 +13,8 @@ using Project.MVC.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
-builder.Services.AddControllersWithViews();
+// Add critical authorization services
+builder.Services.AddAuthorization(); 
 
 // Database Context
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
@@ -21,12 +22,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 // AutoMapper Configuration
 builder.Services.AddAutoMapper(
-    typeof(ServiceMappingProfile),        // Service Layer Mappings
-    typeof(MvcMappingProfile)      // MVC Mappings
+    typeof(ServiceMappingProfile),
+    typeof(MvcMappingProfile)
 );
 
 // Service Registration
 builder.Services.AddScoped<IVehicleService, VehicleService>();
+
+// Add MVC services
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -40,6 +44,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+// Authorization must come after authentication if used
 app.UseAuthorization();
 
 app.MapControllerRoute(
