bc623eaf7ff2fc239e842bf1631e719dc94b2784
Matej Martinović <matt123.3a@gmail.com>
2025-03-02 19:59:00 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.Service/obj/Debug/net8.0/Project.Service.AssemblyInfo.cs
+++ Project.Service/obj/Debug/net8.0/Project.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d807b3323f06c77d86511726c5db7fdaf4545f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bcba91ca2bcd31c92317c504af3eba900d59f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
