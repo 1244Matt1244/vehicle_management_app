80bc89fc087142ee386d2082954873a648f6508f
Matej Martinović <matt123.3a@gmail.com>
2025-03-03 03:18:08 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.MVC/obj/Debug/net9.0/Project.MVC.AssemblyInfo.cs
+++ Project.MVC/obj/Debug/net9.0/Project.MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecacfefbaa69ae1b4d890cb98cf1f8ca2f979d6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e0d3f037f9de728b6a0f604d5bc71b0ae7689e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project.Service/Data/Helpers/PaginatedList.cs
+++ Project.Service/Data/Helpers/PaginatedList.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,12 +22,17 @@ namespace Project.Service.Data.Helpers
         }
 
         public static async Task<PaginatedList<T>> CreateAsync(
-            IQueryable<T> source, int pageIndex, int pageSize)
+            IQueryable<T> source, 
+            int pageIndex, 
+            int pageSize,
+            Func<IQueryable<T>, Task<int>> countAsync)
         {
-            var count = await source.CountAsync();
-            var items = await source.Skip((pageIndex - 1) * pageSize)
-                                    .Take(pageSize)
-                                    .ToListAsync();
+            var count = await countAsync(source);
+            var items = await source
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
