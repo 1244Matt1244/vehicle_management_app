80e0d3f037f9de728b6a0f604d5bc71b0ae7689e
Matej Martinović <matt123.3a@gmail.com>
2025-03-03 02:25:15 +0100

    Clean up: remove unnecessary files and update project structure

--- Project.Service/Mappings/ServiceMappingProfile.cs
+++ Project.Service/Mappings/ServiceMappingProfile.cs
@@ -10,7 +10,8 @@ namespace Project.Service.Mappings
         {
             CreateMap<VehicleMake, VehicleMakeDTO>().ReverseMap();
             CreateMap<VehicleModel, VehicleModelDTO>()
-                .ForMember(dest => dest.MakeName, opt => opt.MapFrom(src => src.Make.Name))
+                .ForMember(dest => dest.MakeName, 
+                    opt => opt.MapFrom(src => src.VehicleMake.Name))
                 .ReverseMap();
         }
     }
